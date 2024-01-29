7d9f0d48f791ab0a543d966d17beef9a681fc695
Madusha Virajika Dissanayaka <madushafin@outlook.com>
2024-01-29 20:14:44 +0200

    Delete contactus.php

--- mvd_Tasks/contactus.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$title = "HAMK - Contact Us";
-include "header.php" ?>
-
-              <h1>HTML Assignment</h1>
-              <p>If you don’t want to go all the way to the North Pole then why not visit Rovaniemi in the north of Finland? Here you will find Christmas delights such as an Elf School and you can even meet Santa and Mrs. Claus who will teach you how to make gingerbread treats.There is a Calligraphy School where you can pen a letter to Santa and a Post Office where the elves will show you all the mail they receive over the course of the year.The main highlight here is to cross the Arctic Circle via an underground tunnel.If you are travelling with children then make sure not to miss this Winter Wonderland experience in Finland.</p>
-             
-<?php include "footer.php" ?>

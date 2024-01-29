f0d385cb51763772ef9e74e0f79fc54ac147cfb8
Madusha Virajika Dissanayaka <madushafin@outlook.com>
2024-01-29 19:57:33 +0200

    Exercise 2 done

---
+++ mvd_Tasks/footer.php
@@ -0,0 +1,14 @@
+</div>
+ </div>
+
+
+    <footer class="row">
+        <div class = "col-md-4"> Madusha Dissanayaka</div>
+        <div class = "col-md-4">Web Development</div>
+        <div class = "col-md-4">2024</div>
+    </footer>
+</div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</body>   
+</body>
+</html>
---
+++ mvd_Tasks/header.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo $title; ?></title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="styles.css">
+</head>
+</head>
+<body>
+    <div class ="container">
+    
+       <div class = "row">
+        <div class = "col-sm-4">
+        <h2>Madusha Dissanayaka</h2>
+        </div>
+        <div class = "col-sm-8">
+            <ul class="nav">
+                <li class="nav-item">
+                  <a class="nav-link active" aria-current="page" href="excersice1.php">Excersice 1</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Excersice 2</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Excersice 3</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Excersice 4</a>
+                </li>
+              </ul>
+                
+        </div>
+
+        <div class = "row">
+       <div class = "col-sm-2">
+        <h1>List of Tasks</h1>
+      <p>some text</p>
+      <button type="button" class="btn btn-primary">Primary</button>
+  </div>
+ <div class = "col-sm-10">
